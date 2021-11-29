11748742a02ff82aa987c2ccf8fa87be1b70cbde
Absaar-Ali <absaarali2256@gmail.com>
2021-11-29 20:45:46 +0530

    First Commit

---
+++ API.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const client = new MongoClient(
+  "mongodb+srv://Absaar:password1548@cluster0.oaxoi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);
+
+client.connect().then(function (mClient) {
+  db = mClient.db();
+  console.log("Mongodb Connection is successful");
+  app.listen(7000, function () {
+    console.log("Server Started on port 7000");
+  });
+});
+
+let obj = [];
+
+app.get("/", function (req, res) {
+  db.collection("urlshortner")
+    .find()
+    .forEach((element) => {
+      obj.push(element);
+    });
+  res.send(obj);
+});
